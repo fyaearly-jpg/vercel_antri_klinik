5d6cfc7a2ca80900821be388c1c7a3895856ec4d
fyaearly-jpg <fyaearly@gmail.com>
2026-04-26 17:04:34 +0700

    Fix koneksi TiDB dan vercel config

---
+++ api/koneksi.php
@@ -0,0 +1,30 @@
+<?php
+// Data dari TiDB Cloud
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = 'wG4jTPVkrLpurY1.root';
+$pass = 'lb5JGw7uy0uZJ9yf';
+$db   = 'klinik_db';
+
+// Inisialisasi mysqli
+$koneksi = mysqli_init();
+
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
+}
+?>
