8759410ac61584726e6b2c200d7a15a9e83c8d20
fyaearly-jpg <fyaearly@gmail.com>
2026-04-27 14:38:31 +0700

    koneksi irul

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,17 +1,30 @@
 <?php
+// Data dari TiDB Cloud
 $host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
 $port = 4000;
 $user = 'wG4jTPVkrLpurY1.root';
 $pass = 'lb5JGw7uy0uZJ9yf';
 $db   = 'klinik_db';
- 
+
+// Inisialisasi mysqli
 $koneksi = mysqli_init();
+
+// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
 mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
-$ok = mysqli_real_connect($koneksi, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
- 
-if (!$ok) {
-    http_response_code(500);
-    die(json_encode(["success" => false, "message" => "Koneksi DB gagal: " . mysqli_connect_error()]));
+
+// Melakukan koneksi
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
 }
-mysqli_set_charset($koneksi, 'utf8mb4');
 ?>
